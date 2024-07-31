7a0003b2dc317fa50818fb8279b955adbaf37648
YongHeeCh <alpacatnt@naver.com>
2024-07-31 12:18:34 +0900

    css3 - 선택자, css리셋

--- app/routes/jscript.py
+++ app/routes/jscript.py
@@ -4,6 +4,6 @@ from starlette.templating import Jinja2Templates
 # 라우터 생성
 jscript_router = APIRouter()
 # 탬플릿 지정
-templetes = Jinja2Templates(directory='views/templates')
+templates = Jinja2Templates(directory='views/templates')
 
 # 라우트 설정
